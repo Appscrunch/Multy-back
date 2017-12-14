2f6fd06dac1aeea093732cf00324d8323e35dda4
Eugene Boltenko <jekabolt@yahoo.com>
2017-12-14 18:39:05 +0300

    Add notifications handlers

--- btc/mempool.go
+++ btc/mempool.go
@@ -1,17 +1,73 @@
 package btc
 
 import (
-	"fmt"
+	"log"
 
+	"github.com/Appscrunch/Multy-back/store"
 	"github.com/btcsuite/btcd/btcjson"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func parseMempoolTransaction(inTx *btcjson.TxRawResult) error {
-	for num, out := range inTx.Vout {
-		fmt.Println("addres", num, out.ScriptPubKey.Addresses)
+func parseMempoolTransaction(inTx *btcjson.TxRawResult) {
+	var user store.User
+	// parse every new transaction out from mempool and notify user with websocket
+	for _, out := range inTx.Vout {
+		for _, address := range out.ScriptPubKey.Addresses {
+
+			query := bson.M{"wallets.addresses.address": address}
+			err := usersData.Find(query).One(&user)
+			if err != nil {
+				continue
+			}
+			log.Printf("[DEBUG] [IS OUR USER] parseMempoolTransaction: usersData.Find = %s", address)
+
+			chToClient <- BtcTransactionWithUserID{
+				UserID: user.UserID,
+				NotificationMsg: &BtcTransaction{
+					TransactionType: txIn,
+					Amount:          out.Value,
+					TxID:            inTx.Txid,
+					Address:         address,
+				},
+			}
+		}
 	}
 
-	return nil
+	// parse every new transaction in from mempool and notify user with websocket
+	for _, input := range inTx.Vin {
+		txHash, err := chainhash.NewHashFromStr(input.Txid)
+		if err != nil {
+			log.Printf("[ERR] parseMempoolTransaction: chainhash.NewHashFromStr = %s", err)
+		}
+		previousTx, err := rpcClient.GetRawTransactionVerbose(txHash)
+		if err != nil {
+			log.Println("ERR GetRawTransactionVerbose [old]: ", err.Error())
+		}
+
+		for _, out := range previousTx.Vout {
+			for _, address := range out.ScriptPubKey.Addresses {
+				query := bson.M{"wallets.addresses.address": address}
+				err := usersData.Find(query).One(&user)
+				if err != nil {
+					continue
+				}
+				log.Printf("[DEBUG] [IS OUR USER]-AS-OUT parseMempoolTransaction: usersData.Find = %s", address)
+
+				chToClient <- BtcTransactionWithUserID{
+					UserID: user.UserID,
+					NotificationMsg: &BtcTransaction{
+						TransactionType: txOut,
+						Amount:          out.Value,
+						TxID:            inTx.Txid,
+						Address:         address,
+					},
+				}
+
+			}
+		}
+	}
+
 }
 
 /*
